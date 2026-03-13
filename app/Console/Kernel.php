bb531382df63c2bb120ab9399a90c988d96a5648
alfredonet99 <avillavicencio@teamnet.com.mx>
2026-03-13 11:11:42 -0600

    se modifica kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-         $schedule->command('guardias:close-expired --minutes=1620')
+         $schedule->command('guardias:close-expired --minutes=1080')
         ->everyTwoHours()
         ->withoutOverlapping()
         ->runInBackground();
